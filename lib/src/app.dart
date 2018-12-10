562d9b1f29af3e1247e004c9f7356b41a0cf57fb
Felipe <feload@gmail.com>
2018-12-09 19:05:37 -0600

    Added button event to add image and title

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import './models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState () {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage () async {
+    counter++;
+    final response = await http.get('https://jsonplaceholder.typicode.com/photos/$counter');
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
   Widget build (context) {
     return MaterialApp(
       home: Scaffold(
+        body: ImageList(images),
         appBar: AppBar(
-          title: Text("Hi there!"),
+          title: Text("Images app!"),
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            print('Hi');
-          },
+          onPressed: fetchImage,
         ),
       )
     );
